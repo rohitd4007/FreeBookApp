995657a0d5d6dd7a3a14aebf5771132f0c629078
rohitd4007 <44453539+rohitd4007@users.noreply.github.com>
2021-06-21 14:15:39 +0530

    updated category component

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
 import pattern from "../icons/Pattern.svg";
+import "./Category.css";
 
-const Category = () => {
+const Category = (props) => {
   return (
     <div className="container">
-      <div className="pattern-container"></div>
-
-      <img src={pattern} alt="pp" />
+      <div className="pattern-container">
+        <img className="pattern" src={pattern} alt="pp" />
+      </div>
+      <div className="title-container">
+        <h2 className="title">Guttenberg Project</h2>
+        <p className="desc">
+          A social cataloging website that allows you to freely serach its
+          database of books, annotate, and review
+        </p>
+      </div>
+      <div className="categories-container">
+        <div className="genre-card">FICTION</div>
+        <div className="genre-card">DRAMA</div>
+        <div className="genre-card">PHILOSOPY</div>
+        <div className="genre-card">HISTORY</div>
+        <div className="genre-card">HUMOUR</div>
+        <div className="genre-card">ADVENTURE</div>
+        <div className="genre-card">POLITICS</div>
+      </div>
     </div>
   );
 };
+
+export default Category;
