f5c148fdbbb3d1afbab6cb95a0084f07fa8b9d8c
rohitd4007 <44453539+rohitd4007@users.noreply.github.com>
2021-06-21 12:36:38 +0530

    initialized project and added components

---
+++ src/Components/Category.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import pattern from "../icons/Pattern.svg";
+
+const Category = () => {
+  return (
+    <div className="container">
+      <div className="pattern-container"></div>
+
+      <img src={pattern} alt="pp" />
+    </div>
+  );
+};
